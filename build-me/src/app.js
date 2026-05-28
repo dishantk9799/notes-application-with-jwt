094fffaa4db11586301de5254df2bf1d8062ff23
Dishant kumawat <dishantk9799@gmail.com>
2026-05-28 23:35:32 +0530

    build-me add api Update note

--- build-me/src/app.js
+++ build-me/src/app.js
@@ -53,4 +53,39 @@ app.get('/api/notes', async (req, res) => {
     }
 });
 
+// ---- Update note ----
+app.patch('/api/notes/:id', async (req, res) => {
+    try {
+
+        // ---- User provided data ----
+        const { id } = req.params;
+        const { description } = req.body;
+
+        // ---- Find user in database
+        const note = await Note.findById(id);
+
+        // ---- Validation ----
+        if (!note) return res.status(404).json({ message: "Note not found" });
+        if (!description) return res.status(400).json({ message: "description is required" });
+        if (description.trim().length < 10) return res.status(400).json({ message: "description should be more then 10 character" });
+
+        // Updating user in database
+        note.description = description;
+        await note.save();
+
+        return res.status(200).json({
+            message: "Note update successfully",
+            note
+        });
+
+    } catch (error) {
+        console.log("Error in Update note:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+});
+
+
+
 export default app;
