7fe604694933d711082795bec8bf41642a283b36
Dishant kumawat <dishantk9799@gmail.com>
2026-05-28 23:20:48 +0530

    build-me add userSchema in models

---
+++ build-me/src/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model("user", userSchema);
